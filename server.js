d5204f662de850fdc647b99fc812a7cfa5df5392
Muhammad Tayyab <tayyabsultan621@gmail.com>
2024-07-14 16:26:28 +0500

    Update server.js

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ app.get("/logout", (req, res) => {
   res.redirect("logged Out");
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+app.listen(process.env.PORT || 8001, () => {
+  console.log("Server is listening on port 8001");
 });
